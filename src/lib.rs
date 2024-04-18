70a6ce8e7fe6622bb68d148bcc0df6c968cf0d32
Fotis Paschos <fpaschos@gmail.com>
2024-04-18 13:19:49 +0300

    First run to consume messages

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,12 @@
 pub mod ingest;
 
 use std::sync::Arc;
+use rdkafka::{ClientConfig, ClientContext};
+use rdkafka::config::RDKafkaLogLevel;
+use rdkafka::consumer::{Consumer, ConsumerContext, StreamConsumer};
 use tokio_util::sync::CancellationToken;
-use tracing::info;
+use tracing::{error, info};
+use url::Url;
 
 #[derive(Debug, thiserror::Error)]
 pub enum IngestError {
@@ -11,21 +15,88 @@ pub enum IngestError {
 }
 
 
-#[derive(Debug, Clone, Default)]
-pub struct IngestOptions {}
+#[derive(Debug, Clone)]
+pub struct IngestOptions {
+    /// The Kafka broker string to connect to.
+    pub kafka_brokers: String,
+    /// The Kafka consumer group id to set to allow for multiple consumers per topic.
+    pub consumer_group_id: String,
+    /// Input format
+    pub input_format: MessageFormat,
+}
 
+impl Default for IngestOptions {
+    fn default() -> Self {
+        Self {
+            kafka_brokers: "localhost:9092".to_string(),
+            consumer_group_id: "kafka-delta-ingest".to_string(),
+            input_format: MessageFormat::Protobuf(SchemaSource::None),
+        }
+    }
+}
+
+/// Formats for message parsing
+#[derive(Clone, Debug)]
+pub enum MessageFormat {
+    Json(SchemaSource),
+    // TODO remove propably
+    Protobuf(SchemaSource),
+}
+
+#[derive(Clone, Debug)]
+pub enum SchemaSource {
+    None,
+    SchemaRegistry(Url),
+}
+
+
+pub struct KafkaContext;
+
+impl ClientContext for KafkaContext {
+}
+
+impl ConsumerContext for KafkaContext {}
 
 pub async fn start_ingest(
     topic: String,
     opts: IngestOptions,
-    cancellation_token: Arc<CancellationToken>
+    cancellation_token: Arc<CancellationToken>,
 ) -> Result<(), IngestError> {
     info!("Starting ingest for topic: {}", topic);
 
+    // TODO separate method kafka config from opts
+    // Create the `StreamConsumer`, to receive the messages from the topic in form of a `Stream`.
+    let consumer: StreamConsumer<KafkaContext> = ClientConfig::new()
+        .set("group.id", &opts.consumer_group_id)
+        .set("bootstrap.servers", &opts.kafka_brokers)
+        .set("enable.partition.eof", "false")
+        .set("session.timeout.ms", "6000")
+        .set("enable.auto.commit", "false")
+        .set("auto.offset.reset", "earliest")
+        .create_with_context(KafkaContext).map_err(|_e| {
+        IngestError::IngestError
+    })?;
+
+    consumer.subscribe(&[topic.as_str()]).map_err(|_e| {
+        IngestError::IngestError
+    })?;
+
     // The run loop
     loop {
-        if cancellation_token.is_cancelled() {
-            return Ok(());
+        tokio::select! {
+            _ = cancellation_token.cancelled() => {
+                return Ok(());
+            }
+            consumer_result = consumer.recv() => {
+                match consumer_result {
+                    Ok(message) => {
+                        info!("Received message: {:?}", message);
+                    }
+                    Err(e) => {
+                        error!("Error while consuming message: {:?}", e);
+                    }
+                }
+            }
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,39 @@
 use std::sync::Arc;
 use tokio_util::sync::CancellationToken;
 use tracing::{error, info};
-use kafka_delta_proto_ingest::start_ingest;
+use kafka_delta_proto_ingest::{IngestOptions, MessageFormat, start_ingest};
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
+use kafka_delta_proto_ingest::SchemaSource::SchemaRegistry;
 
 // TODO add clap and cli commands
 // TODO add tracing json via cli param see: https://github.com/tokio-rs/tracing/blob/master/examples/examples/toggle-subscribers.rs
 
-const TOPIC: &str = "topic";
+const TOPIC: &str = "proto.ds.claim";
+const SCHEMA_REGISTRY_URL: &str = "http://localhost:58085/";
+
+const KAFKA_BROKERS: &str = "localhost:59092";
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-
     let plain = tracing_subscriber::fmt::layer();
     tracing_subscriber::registry().with(plain).init();
 
+    let opts = IngestOptions {
+        kafka_brokers: KAFKA_BROKERS.to_string(),
+        consumer_group_id: format!("{}-delta-ingest", TOPIC),
+        input_format: MessageFormat::Protobuf(
+            SchemaRegistry(url::Url::parse(SCHEMA_REGISTRY_URL)?,
+            ),
+        ),
+
+    };
 
     let cancellation = Arc::new(CancellationToken::new());
     let res = tokio::spawn({
         let cancellation = cancellation.clone();
         async move {
-            let res = start_ingest(TOPIC.to_string(), Default::default(), cancellation.clone()).await;
+            let res = start_ingest(TOPIC.to_string(), opts, cancellation.clone()).await;
             cancellation.cancel();
             res
         }
@@ -38,7 +50,7 @@ async fn main() -> anyhow::Result<()> {
         }
     }
 
-    if let Err(err) =  res.await? {
+    if let Err(err) = res.await? {
         error!("Ingest run failed: {:?}", err);
     } else {
         info!("Ingest run finished gracefully");
